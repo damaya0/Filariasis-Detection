45205bee5b5b04a8cad62231dc23b3313ac4ea5b
Damsith Adikari <damsithadikari2001@gmail.com>
2026-01-27 07:25:29 +0530

    environment testing

---
+++ scripts/test.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+print(f"TensorFlow Version: {tf.__version__}")
+
+from object_detection.utils import label_map_util
+print("Object Detection API loaded successfully!")
+exit()
