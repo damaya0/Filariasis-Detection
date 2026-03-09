7d2e8f38f7a56e17550c0dfc54bacf54f532486d
Damsith Adikari <damsithadikari2001@gmail.com>
2026-03-09 19:25:57 +0530

    trained repo

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print(f"TensorFlow Version: {tf.__version__}")
-
-from object_detection.utils import label_map_util
-print("Object Detection API loaded successfully!")
-exit()
